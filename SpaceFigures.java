ac813b68836760afe66aa445560338a3e2f0d74c
ingrid <kamilaingridc@gmail.com>
2024-03-07 16:18:34 -0300

    'initial'

---
+++ SpaceFigures.java
@@ -0,0 +1,5 @@
+public class SpaceFigures {
+    public static void main(String args[]) {
+
+    }
+}
---
+++ flatFigures.java
@@ -0,0 +1,5 @@
+public class flatFigures {
+    public static void main(String args[]) {
+
+    }
+}
