522922a8c7b28982572becb042dd8041deedc8b1
ingrid <kamilaingridc@gmail.com>
2024-03-07 16:48:52 -0300

    javajava

--- Main.java
+++ Main.java
@@ -1,6 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         System.out.println("GEOMETRIC CALCULATOR +-/*");
-        System.out.println("");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("[1] Calculate plane figures | [2] Calculate spatial figures | [3] Exit");
+        Integer choice = scanner.nextInt();
+        switch (choice) {
+            case 1:
+
+            case 2:
+
+            case 3:
+                System.out.println("Leaving...");
+                break;
+            default:
+                System.out.println("Invalid choice. Try again: ");
+                Main.main();
+        }
+        System.out.println(choice);
+       // switch ()
     }
 }
--- flatFigures.java
+++ flatFigures.java
@@ -1,5 +1,9 @@
 public class flatFigures {
     public static void main(String args[]) {
+        System.out.println("Choose the figure u'd like to calculate:");
+        System.out.println("Square | Triangle | ");
+    }
+    class Square(double number, double number2){
 
     }
 }
